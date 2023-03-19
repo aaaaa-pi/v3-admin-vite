a036eda98a4b4683d7b274e725650933d9bfef51
Eureka <euraka77035@gmail.com>
2023-03-20 06:41:59 +0800

    表格数据的初步展示

--- src/view/main/system/user/cpns/user-content.vue
+++ src/view/main/system/user/cpns/user-content.vue
@@ -6,9 +6,58 @@
         <el-button type="primary" size="large"> 新建用户 </el-button>
       </div>
       <div class="table">
-        <ul v-for="item in usersList" :key="item.name">
-          <li>{{ item.name }}</li>
-        </ul>
+        <el-table :data="usersList" border style="width: 100%">
+          <el-table-column align="center" type="selection" width="55" />
+          <el-table-column
+            align="center"
+            type="index"
+            label="序号"
+            width="60px"
+          />
+          <el-table-column
+            align="center"
+            prop="name"
+            label="用户名"
+            width="220px"
+          />
+          <el-table-column
+            align="center"
+            prop="realname"
+            label="真实姓名"
+            width="150px"
+          />
+          <el-table-column
+            align="center"
+            prop="cellphone"
+            label="手机号码"
+            width="160px"
+          />
+          <el-table-column
+            align="center"
+            label="状态"
+            prop="enable"
+            width="100px"
+          >
+            <template #default="scope">
+              <el-button
+                size="small"
+                :type="scope.row.enable ? 'primary' : 'danger'"
+                plain
+                >{{ scope.row.enable ? '启用' : '禁用' }}
+              </el-button>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" prop="creatAt" label="创建时间" />
+          <el-table-column align="center" prop="updateAt" label="更新时间" />
+          <el-table-column align="center" label="操作" width="160px">
+            <el-button size="small" icon="Edit" type="primary" text>
+              编辑
+            </el-button>
+            <el-button size="small" icon="Delect" type="danger" text>
+              删除
+            </el-button>
+          </el-table-column>
+        </el-table>
       </div>
     </el-card>
   </div>
@@ -32,6 +81,12 @@ const { usersList } = storeToRefs(systemStore)
     display: flex;
     justify-content: space-between;
     align-items: flex-end;
+  }
+}
+.table {
+  margin-top: 24px;
+  .el-button {
+    margin-left: 0;
   }
 }
 </style>
